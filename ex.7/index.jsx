33112d974ea748189ea196f8dd7021a0a406cd09
Roger-Franco <rogerfrancogalt@gmail.com>
2022-07-28 08:57:49 -0400

    Componente com Estado

--- ex.7/index.jsx
+++ ex.7/index.jsx
@@ -4,6 +4,6 @@ import ClassComponent from "./classComponent";
 
 ReactDOM.render(
   <div>
-    <ClassComponent label="Contador" initialValue={10} />
+    <ClassComponent value="Componente Classe" />
   </div>
   , document.getElementById("app"));
